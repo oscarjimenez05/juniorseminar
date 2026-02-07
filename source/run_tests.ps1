bd693bdfd7fa98fcc868b20bf7bd102ec0c618e2
oscar <ojimenezrodriguez@ithaca.edu>
2026-02-07 17:05:16 -0500

    attempting script restructure (BUGGED)

--- source/run_tests.ps1
+++ source/run_tests.ps1
@@ -2,7 +2,6 @@ param(
     [switch]$BigCrush = $false
 )
 
-
 $SEED = 2366022249
 $IMAGE_NAME = "marzopa/prng-bench"
 $RESULTS_DIR = "${PWD}/results"
@@ -10,28 +9,25 @@ $RESULTS_DIR = "${PWD}/results"
 if (!(Test-Path $RESULTS_DIR)) { New-Item -ItemType Directory -Path $RESULTS_DIR }
 
 function Launch-Test ($name, $algo, $delta) {
-    Write-Host "Launching $name..." -NoNewline
-
+    $name = $name + "_" + $SEED
     if ($BigCrush){
-        $cmd = "python3 testing_interface.py p $SEED $delta --algo $algo | ./test_from_pipe_BigCrush"
+        $cmd = "python3 testing_interface.py p $SEED $delta --algo $algo | ./test_from_pipe_BigCrush > $reportFile"
+        $reportFile = "/app/results/${name}_BigCrush.txt"
     }
     else{
-        $cmd = "python3 testing_interface.py p $SEED $delta --algo $algo | ./test_from_pipe"
+        $cmd = "python3 testing_interface.py p $SEED $delta --algo $algo | ./test_from_pipe > $reportFile"
+        $reportFile = "/app/results/${name}_Crush.txt"
     }
+
+    Write-Host "Launching $name... (Report will save to results/${name}_report.txt)"
 
     $containerName = "${name}_run"
 
     docker rm -f $containerName 2>$null | Out-Null
-
-    $id = docker run -d --name $containerName $IMAGE_NAME sh -c $cmd
-
-    # Start a background process to tail the logs into your local file
-    Start-Job -ScriptBlock {
-        param($cName, $resDir, $tName)
-        docker logs -f $cName > "$resDir/${tName}_report.txt" 2> "$resDir/${tName}_progress.log"
-    } -ArgumentList $containerName, $RESULTS_DIR, $name | Out-Null
-
-    Write-Host " [OK] Monitoring Seed: $SEED"
+    docker run --rm --name $containerName `
+        -v "${PWD}/results:/app/results" `
+        $IMAGE_NAME `
+        sh -c $cmd
 }
 
 Write-Host "--- Starting Parallel Tests ---" -ForegroundColor Cyan
